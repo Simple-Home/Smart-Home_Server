2905e064b8f6ebb0a14868a9996b7c3d8b01f5d6
GamerClassN7 <gamerclassn7@gmail.com>
2021-03-02 17:40:23 +0100

    New Project

---
+++ PlatformIO/New-Unified-Firmware-Optimalized/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <config.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  //connect wifi
+  //translate server to IP and port
+  //send diag to server
+  //comunication ower https
+
+  while (/* wifi conected */)
+  {
+    /* loop */
+  }
+}
