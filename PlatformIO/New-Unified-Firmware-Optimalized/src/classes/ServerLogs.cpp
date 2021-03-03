8c1f93fa6f82871ed7478a49fcbafdd85a5a8338
GamerClassN7 <gamerclassn7@gmail.com>
2021-03-03 14:05:35 +0100

    Error Removal

--- PlatformIO/New-Unified-Firmware-Optimalized/src/classes/ServerLogs.cpp
+++ PlatformIO/New-Unified-Firmware-Optimalized/src/classes/ServerLogs.cpp
@@ -6,15 +6,15 @@ ServerLogs::ServerLogs(){
 
 void ServerLogs::add(String msg)
 {
-    this->payload.add(msg);
+    //Add String to Array/json document Payload
+    //this->payload.add(msg);
 }
 
 bool ServerLogs::send()
 {
+    //if payload is not empty
     if (this->payload){
-        #ifdef ENABLE_SERIAL_PRINT
-            Serial.println("Logs-json-Error: " + String(jsonError.c_str()));
-        #endif
-        }
-        return false;
+        //send payload to server
+    }
+    return false;
 }
