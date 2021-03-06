9328137d7a8a9bd08ddcbf3ad772beada5426305
GamerClassN7 <gamerclassn7@gmail.com>
2021-03-06 20:12:31 +0100

    Main Twek

--- PlatformIO/New-Unified-Firmware-Optimalized/src/main.cpp
+++ PlatformIO/New-Unified-Firmware-Optimalized/src/main.cpp
@@ -8,32 +8,40 @@
 EepromManager eeprom_storage;
 WifiManager wifi_conection;
 
-void setup() {
-  #ifdef ENABLE_SERIAL_PRINT
-    Serial.begin(115200);
-    while (!Serial) continue;
-  #endif
-  if (true) {
-    eeprom_storage.write((char*)WIFI_SSID, 1);
-    eeprom_storage.write((char*)WIFI_PASS, 33);
+void setup()
+{
+#ifdef ENABLE_SERIAL_PRINT
+  Serial.begin(115200);
+  while (!Serial)
+    continue;
+#endif
+  if (true)
+  {
+    eeprom_storage.write((char *)WIFI_SSID, 1);
+    eeprom_storage.write((char *)WIFI_PASS, 33);
   }
 }
 
-void loop() {
-  wifi_conection.connect(eeprom_storage.read(1,33), eeprom_storage.read(33, 65));
+void loop()
+{
+  wifi_conection.connect(eeprom_storage.read(1, 33), eeprom_storage.read(33, 65));
   //translate server to IP and port
   //send diag to server
   //comunication ower https
 
   while (wifi_conection.check())
   {
-    char* token = eeprom_storage.read(65, 97);
-    HttpManager http_conection((char*)"https://dev.steelants.cz", (char*)"", (char*)"/vasek/home-milanin/api/v2/endpoint", token);
-    if (http_conection.connect()){
-      http_conection.send((char*)"test");
+    if (!wifi_conection.check())
+      continue;
+
+    char *token = eeprom_storage.read(65, 97);
+    HttpManager http_conection((char *)"https://dev.steelants.cz", (char *)"", (char *)"/vasek/home-milanin/api/v2/endpoint", token);
+    if (http_conection.connect())
+    {
+      http_conection.send((char *)"test");
       String payload = http_conection.getPayload();
       http_conection.disconect();
     }
-    delay(100);
   }
+  delay(100);
 }
