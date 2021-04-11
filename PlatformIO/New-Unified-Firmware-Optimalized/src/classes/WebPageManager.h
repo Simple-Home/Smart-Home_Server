6606025352b5cfa97b7e95690c0a336af93ba427
GamerClassN7 <gamerclassn7@gmail.com>
2021-04-11 21:19:44 +0200

    Fix

--- PlatformIO/New-Unified-Firmware-Optimalized/src/classes/WebPageManager.h
+++ PlatformIO/New-Unified-Firmware-Optimalized/src/classes/WebPageManager.h
@@ -8,27 +8,28 @@
 #include <DNSServer.h>
 #include <ESP8266WiFi.h>
 
-class WebPageManager{
-    private:
-        bool configPage = false;
-        ESP8266WebServer server(80);
-        const byte DNS_PORT = 53;
-        DNSServer dnsServer;
-        String configApName;
-        String configApPassword;
-        String pageContent;
-        String styleContent;
-        String scriptContent;
-        String pageHtml;
-        EepromManager eeprom_storage;
+class WebPageManager
+{
+private:
+    bool configPage = false;
+    ESP8266WebServer server{80};
+    const byte DNS_PORT = 53;
+    DNSServer dnsServer;
+    String configApName;
+    String configApPassword;
+    String pageContent;
+    String styleContent;
+    String scriptContent;
+    String pageHtml;
+    EepromManager eeprom_storage;
 
-    public:
-        WebPageManager(String configApName, String configApPassword, EepromManager eeprom_storage);
-        void StartPage();
-        void ConfigPage();
-        void ResponseHandler();
-        String wifiScan();
-        void Active();
+public:
+    WebPageManager(String configApName, String configApPassword, EepromManager eeprom_storage);
+    void StartPage();
+    void ConfigPage();
+    void ResponseHandler();
+    String wifiScan();
+    void Active();
 };
 
 #endif
