7c5d25bfd74ae4b76cf0e073064973df8e064a07
JonatanRek <vasek@steelants.cz>
2021-12-10 16:48:04 +0100

    Shelly 1 Test Config 2

--- PlatformIO/Unified-Firmware/src/config_adv.h
+++ PlatformIO/Unified-Firmware/src/config_adv.h
@@ -7,26 +7,39 @@
 #include <ArduinoJson.h>
 
 #ifdef WIFI_CONFIG_PAGE
-    #include <ESP8266WebServer.h>
-    #include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
 #endif
 
 #if defined(ENCODE_WIFI_SETING_TO_CODE) && defined(WIFI_CONFIG_PAGE)
-    #error "Define only one of ENCODE_WIFI_SETING_TO_CODE/WIFI_CONFIG_PAGE"
-    #error "ENCODE_WIFI_SETING_TO_CODE - hard Encode wifi ccredentials to Code"
-    #error "WIFI_CONFIG_PAGE - Enable usage of EEPROM"
+#error "Define only one of ENCODE_WIFI_SETING_TO_CODE/WIFI_CONFIG_PAGE"
+#error "ENCODE_WIFI_SETING_TO_CODE - hard Encode wifi ccredentials to Code"
+#error "WIFI_CONFIG_PAGE - Enable usage of EEPROM"
 #endif
 
 #ifdef DHT_PIN
-    #include <Adafruit_Sensor.h>
-    #include <DHT.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 #endif
 
 #ifdef SWITCH1_PIN
-    #if defined(MOMENTARY_SWITCH) && defined(ON_OFF_SWITCH)
-        #error "Define only one type MOMENTARY_SWITCH/ON_OFF_SWITCH"
-    #endif
-    #if !defined(MOMENTARY_SWITCH) && !defined(ON_OFF_SWITCH)
-        #error "Type of switch need to be defined MOMENTARY_SWITCH/ON_OFF_SWITCH"
-    #endif
+#if defined(MOMENTARY_SWITCH) && defined(ON_OFF_SWITCH)
+#error "Define only one type MOMENTARY_SWITCH/ON_OFF_SWITCH"
 #endif
+#if !defined(MOMENTARY_SWITCH) && !defined(ON_OFF_SWITCH)
+#error "Type of switch need to be defined MOMENTARY_SWITCH/ON_OFF_SWITCH"
+#endif
+#endif
+
+// #define D0 16;
+// #define D1 5;
+// #define D2 4;
+// #define D3 0;
+// #define D4 2;
+// #define D5 14;
+// #define D6 12;
+// #define D7 13;
+// #define D8 15;
+// #define D9 3;
+// #define D10 1;
+// #define LED_BUILTIN 16
--- PlatformIO/Unified-Firmware/src/helpers/HTTP.h
+++ PlatformIO/Unified-Firmware/src/helpers/HTTP.h
@@ -6,22 +6,27 @@ String sendHttpRequest(String requestJson, String token)
   client.setInsecure();
   https.begin(client, String(apiHost + apiUrl));
   https.setReuse(true);
+
+#ifdef ARDUINO_ESP8266_ESP_WROOM_02
   https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+#else
+  https.setFollowRedirects(2);
+#endif
 
   https.setRedirectLimit(1);
   https.addHeader("Content-Type", "application/json");
-  
+
   int httpsCode = https.POST(requestJson);
   String payload = "";
-  #ifdef ENABLE_SERIAL_PRINT
-    Serial.print("<- HTTP - " + (String) httpsCode);
-  #endif
+#ifdef ENABLE_SERIAL_PRINT
+  Serial.print("<- HTTP - " + (String)httpsCode);
+#endif
   if (httpsCode > 0)
   {
     payload = https.getString();
-    #ifdef ENABLE_SERIAL_PRINT
-      Serial.println(" - " + payload);
-    #endif
+#ifdef ENABLE_SERIAL_PRINT
+    Serial.println(" - " + payload);
+#endif
   }
   https.end();
   return payload;
@@ -32,20 +37,21 @@ bool sendData(DynamicJsonDocument requestJson, String token)
   requestJson["token"] = token;
   String jsonString = "";
   serializeJson(requestJson, jsonString);
-  #ifdef ENABLE_SERIAL_PRINT
-    Serial.println("-> " + jsonString);
-  #endif
+#ifdef ENABLE_SERIAL_PRINT
+  Serial.println("-> " + jsonString);
+#endif
   String response = sendHttpRequest(jsonString, token);
 
   if (response.length() > 1)
   {
     jsonError = deserializeJson(jsonObject, response);
-    if (jsonError.code() == DeserializationError::Ok){
+    if (jsonError.code() == DeserializationError::Ok)
+    {
       return true;
     }
-    #ifdef ENABLE_SERIAL_PRINT
-      Serial.println("Json Deserialize Error: " + String(jsonError.c_str()));
-    #endif
+#ifdef ENABLE_SERIAL_PRINT
+    Serial.println("Json Deserialize Error: " + String(jsonError.c_str()));
+#endif
   }
   return false;
 }
