47b3ec4b902c00addc948ec425385f9185f5176b
Václav Španinger <vasek@steelants.cz>
2021-12-20 06:26:24 +0000

    Update 'PlatformIO/Unified-Firmware/src/helpers/EEPROM.h'

--- PlatformIO/Unified-Firmware/src/helpers/EEPROM.h
+++ PlatformIO/Unified-Firmware/src/helpers/EEPROM.h
@@ -2,14 +2,17 @@ void WriteEeprom(String data, int start = 1, bool force = false)
 {
   for (int i = 0; i < (int)data.length(); ++i)
   {
-    EEPROM.write(start + i, data[i]);
+    EEPROM.write(start + i, data.charAt(i));
     #ifdef ENABLE_SERIAL_PRINT
-      Serial.println("Writing EEPROM" + data[i]);
+      Serial.println("Writing EEPROM" + data.charAt(i));
     #endif
     delay(100);
   }
   if (force == true){
-    EEPROM.commit();
+    bool status = EEPROM.commit();
+    #ifdef ENABLE_SERIAL_PRINT
+      Serial.println((status) ? "EEPROM - Commit OK" : "EEPROM - Commit failed");
+    #endif
   }
 }
 
