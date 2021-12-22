bf2a673815e918ace2c86e192bbeba2e2f5183a1
GamerClassN7 <gamerclassn7@gmail.com>
2021-12-22 22:29:46 +0100

    Mmory Conservation need Testing

--- PlatformIO/Unified-Firmware/src/helpers/HTTP.h
+++ PlatformIO/Unified-Firmware/src/helpers/HTTP.h
@@ -1,58 +1,50 @@
 HTTPClient https;
 String response;
 
-String sendHttpRequest(String requestJson, String token)
+bool sendHttpRequest(String requestJson, String token)
 {
   client.setInsecure();
   https.setTimeout(15000);
   https.begin(client, String(apiHost + apiUrl));
   https.setReuse(true);
-
 #ifdef ARDUINO_ESP8266_ESP_WROOM_02
   https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
 #else
   https.setFollowRedirects(2);
 #endif
-
-  https.setRedirectLimit(1);
   https.addHeader("Content-Type", "application/json");
 
   int httpsCode = https.POST(requestJson);
-  String payload = "";
 #ifdef ENABLE_SERIAL_PRINT
   Serial.print("<- HTTP - " + (String)httpsCode);
 #endif
   if (httpsCode > 0)
   {
-    payload = https.getString();
 #ifdef ENABLE_SERIAL_PRINT
-    Serial.println(" - " + payload);
+    Serial.println(" - " + https.getString());
+#endif
+    jsonError = deserializeJson(jsonObject, https.getStream());
+    if (jsonError.code() == DeserializationError::Ok)
+    {
+      https.end();
+      return true;
+    }
+#ifdef ENABLE_SERIAL_PRINT
+    Serial.println("Json Deserialize Error: " + String(jsonError.c_str()));
 #endif
   }
   https.end();
-  return payload;
+  return false;
 }
 
 bool sendData(DynamicJsonDocument requestJson, String token)
 {
-    requestJson["token"] = token;
-    String jsonString = "";
-    serializeJson(requestJson, jsonString);
-    #ifdef ENABLE_SERIAL_PRINT
-      Serial.println("-> " + jsonString);
-    #endif
-    String response = sendHttpRequest(jsonString, token);
-
-    if (response.length() > 1)
-    {
-      jsonError = deserializeJson(jsonObject, response);
-      if (jsonError.code() == DeserializationError::Ok)
-      {
-        return true;
-      }
-      #ifdef ENABLE_SERIAL_PRINT
-          Serial.println("Json Deserialize Error: " + String(jsonError.c_str()));
-      #endif
-    }
-    return false;
+  requestJson["token"] = token;
+  String jsonString = "";
+  serializeJson(requestJson, jsonString);
+#ifdef ENABLE_SERIAL_PRINT
+  Serial.println("-> " + jsonString);
+#endif
+  sendHttpRequest(jsonString, token);
+  return false;
 }
